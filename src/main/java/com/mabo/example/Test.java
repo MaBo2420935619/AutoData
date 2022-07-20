4991e707cb14a8e0aead36f419a92a201288e5df
mabo <mabo_9704@163>
2022-07-20 11:33:02 +0800

    新增数据库操作工具

--- src/main/java/com/mabo/example/Test.java
+++ src/main/java/com/mabo/example/Test.java
@@ -1,11 +1,20 @@
 package com.mabo.example;
 
-import com.mabo.example.entity.TestEntity;
-import com.mabo.sql.CreateSqlReflect;
+import com.mabo.sql.utils.JdbcTemplate;
+
+import java.util.HashMap;
+import java.util.List;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        String test = CreateSqlReflect.createSql(TestEntity.class, "TEST", 10);
-        System.out.println(test);
+//        String test = CreateSqlReflect.createSql(TestEntity.class, "TEST", 10);
+//        System.out.println(test);
+
+
+        List<HashMap<String, Object>> hashMaps = JdbcTemplate.queryForList("select * from history");
+        System.out.println(hashMaps);
+        int update = JdbcTemplate.update("update history set ip='231213213' where ip= '111'");
+        System.out.println(update);
+        JdbcTemplate.update("insert into history VALUES ('231213213', '123', '2022-06-29 11:03:40', 1)");
     }
 }
