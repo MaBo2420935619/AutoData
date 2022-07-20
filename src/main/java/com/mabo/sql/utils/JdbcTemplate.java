8a6e86ec8f63e5ced481b8b8cb6ba6cfbc086012
mabo <mabo_9704@163>
2022-07-20 11:38:45 +0800

    新增数据库操作工具

--- src/main/java/com/mabo/sql/utils/JdbcTemplate.java
+++ src/main/java/com/mabo/sql/utils/JdbcTemplate.java
@@ -17,7 +17,7 @@ public class JdbcTemplate {
         try {
             String url = "jdbc:mysql://localhost:3306/downloadsdk?useSSL=false&serverTimezone=GMT%2B8&useUnicode=true&characterEncoding=UTF8&autoReconnect=true&failOverReadOnly=false";
             String username="root";
-            String password="368541186MaBo123";
+            String password="xxx";
             //2.创建数据源对象
             /*DataSource:是SUN公司声明的接口 。DruidDataSource：是阿里巴巴对应实现的类*/
             //3.设置属性
